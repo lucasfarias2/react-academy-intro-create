17a5cd6f0d404081d7b61369b439ca83940a6639
Lucas Farias <lukf95@gmail.com>
2018-10-13 18:47:42 -0300

    ReactDOM.render

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      React.createElement('div', { className: "App" }, "Hello meli")
+      <div className="App">
+        Hello world!
+      </div>
     );
   }
 }
